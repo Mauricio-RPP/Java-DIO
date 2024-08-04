7edb2a7d9759e7c0c1456e4704390eae484d31b4
MauricioRPP <mauriciorpp@hotmail.com>
2024-08-04 18:55:04 -0300

    First commit

---
+++ Hello/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num = 1;
+        num = soma(1,2);
+        System.out.println("Hello, WSL! " + num);
+    }
+
+    public static int soma (int num1, int num2) {
+        return num1 + num2;       
+    }
+}
